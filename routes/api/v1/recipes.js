2eda13d63e63f376df80976f72170858715b4f39
Maddie <meffa2005@gmail.com>
2025-11-24 14:36:14 -0600

    working in Exam 3

---
+++ routes/api/v1/recipes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router()
+const recipes = require('../../../data/recipes.json')
+
+//get elements from all the recipes
+router.get('/api/v1', (_, response) =>{
+    const summaries = recipes.map(recipe =>{
+        const {id, title, image, prepTime, difficulty} = recipe
+        return {id, title, image, prepTime, difficulty}
+    })
+    response.send(summaries)
+})
+
+//get full recipe with a matching id
+router.get('/api/v1/recipes/:id', (request, response) =>{
+
+    const {id} = request.params
+
+    const found = recipes.find(recipe =>{
+        return recipe.id.toString() === id
+    })
+
+    if (found) return response.send(found)
+    response.status(404).send({error: 'Could not find recipe with id '+id})
+})
+
+//add recipe to the array
+
+router.get('/api/v1/recipes/add', (request, response) =>{
+    
+    const id=recipes.length+1 //new id
+    const recipe = request.body
+    const newRecipe = {id, ...recipe}//new id with the contents of the recipe
+    recipes.push(newRecipe)//push the new recipe
+    response.send(newRecipe)
+})
+
+//return the routes
+module.exports = router
